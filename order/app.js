60f77b46872807078147c3ac961ba1a8e74d73c2
InnomindAdmin <417558659@qq.com>
2019-07-24 15:05:49 +0800

    add log and error handling modult

--- order/app.js
+++ order/app.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan")
 
 const productRouters = require("./api/routers/products");
 const orderRouters = require("./api/routers/orders");
 
+app.use(morgan("dev"));
+
 app.use('/products', productRouters);
 app.use('/orders', orderRouters);
+
+app.use((req, res, next) => {
+    let error = new Error("Not Found");
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error:error.message
+    })
+
+});
 
 module.exports = app;
